13dfced70740a4995dafab4d90e264d575dd5c1e
Mateusz <mateusz.kalinowski@icloud.com>
2017-04-21 22:53:34 +0200

    Prace nad przepisaniem samego emulatora na tryb bardziej realistyczny

--- src/microcontroller/Cpu.java
+++ src/microcontroller/Cpu.java
@@ -19,13 +19,6 @@ import java.util.Map;
 public class Cpu {
 
     public Cpu(){
-        knownMnemonics.add(new Mnemonic("MOV",2,2,2));//OK
-        knownMnemonics.add(new Mnemonic("DJNZ",2,2,2));//OK
-        knownMnemonics.add(new Mnemonic("LJMP",1,2,3));//OK
-        knownMnemonics.add(new Mnemonic("RL",1,1,1));//OK
-        knownMnemonics.add(new Mnemonic("RR",1,1,1));//OK
-        knownMnemonics.add(new Mnemonic("RLC",1,1,1));
-        knownMnemonics.add(new Mnemonic("RRC",1,1,1));
         resetCpu();
     }
 
@@ -64,120 +57,91 @@ public class Cpu {
     }
 
     public void executeInstruction() throws InstructionException{
-        Instruction toExecute = codeMemory.getFromAddress(linePointer);
-        if(toExecute.getMnemonic().equals("LJMP")) {
-            int numberLenght = toExecute.getParam1().length();
-            int jump = 0;
-            String number = toExecute.getParam1();
-            if(number.charAt(numberLenght-1) == 'D') {
-                jump = Integer.parseInt(number.substring(0,numberLenght-1));
-            }
-            else if(number.charAt(numberLenght-1) == 'H') {
-                jump = Integer.parseInt(number.substring(0,numberLenght-1),16);
-            }
-            else if(number.charAt(numberLenght-1) == 'B') {
-                jump = Integer.parseInt(number.substring(0,numberLenght-1),2);
-            }
-            else {
-                int jumpNumber = codeMemory.getLineFromLabel(toExecute.getParam1());
-                if (jumpNumber==-1) {
-                    throw new InstructionException();
-                }
-                else
-                    jump = jumpNumber;
 
-            }
-            linePointer = jump;
+        String toExecute = codeMemory.getFromAddress(linePointer);
+        if(toExecute.equals("00000010")) { //LJMP
+            machineCycle();
+            machineCycle();
+            linePointer = Integer.parseInt(codeMemory.getFromAddress(linePointer+1) + codeMemory.getFromAddress(linePointer+2));
         }
-        else if (toExecute.getMnemonic().equals("MOV")) {
-            if(toExecute.getParam2().charAt(0) == '#') {
-                String numberToAdd = toExecute.getParam2();
-                int numberToAddLength = numberToAdd.length();
-                int intNumberToAdd =-1;
-                if(numberToAdd.charAt(numberToAddLength-1)=='D') {
-                    intNumberToAdd = Integer.parseInt(numberToAdd.substring(1,numberToAddLength-1));
-                }
-                else if(numberToAdd.charAt(numberToAddLength-1)=='H') {
-                    intNumberToAdd = Integer.parseInt(numberToAdd.substring(1,numberToAddLength-1),16);
-                }
-                else if(numberToAdd.charAt(numberToAddLength-1)=='B') {
-                    intNumberToAdd = Integer.parseInt(numberToAdd.substring(1,numberToAddLength-1),2);
-                }
-                else {
-                    System.out.println("Niepomyślne analizowanie liczby");
-                    throw new InstructionException();
-                }
-
-                if(registers.containsKey(toExecute.getParam1())) {
-                    registers.put(toExecute.getParam1(),intNumberToAdd);
-                }
-                else {
-                    if(ports.containsKey(toExecute.getParam1())) {
-                        ports.put(toExecute.getParam1(),intNumberToAdd);
-                    }
-                    else
-                        throw new InstructionException();
-                }
-
+        else if(toExecute.substring(0,5).equals("11011")) { //DJNZ Rx,label/adres (ale na wartosci a nie offsecie)
+            machineCycle();
+            machineCycle();
+            int rejestr = Integer.parseInt(toExecute.substring(5,7),2);
+            String rejestrString = "R" + rejestr;
+            int wartosc = registers.get(rejestrString);
+            wartosc--;
+            if(wartosc==-1)
+                wartosc=255;
+            if(wartosc>0) {
+                linePointer = Integer.parseInt(codeMemory.getFromAddress(linePointer+1),2);
             }
-            else if(toExecute.getParam1().equals("A")) {
-                try {
-                    if(registers.containsKey(toExecute.getParam2())) {
-                        int intNumberToAdd = registers.get(toExecute.getParam2());
-                        registers.put("A", intNumberToAdd);
-                    }
-                    else if(ports.containsKey(toExecute.getParam2())) {
-                        int intNumberToAdd = ports.get(toExecute.getParam2());
-                        registers.put("A", intNumberToAdd);
-                    }
-
-                } catch (Exception e) {
-                    System.out.println("Błąd w mov");
-                    throw new InstructionException();
-                }
-            }
-            else if(toExecute.getParam2().equals("A")) {
-                try {
-                    int intNumberToAdd = registers.get("A");
-                    if(registers.containsKey(toExecute.getParam1())) {
-                        registers.put(toExecute.getParam1(), intNumberToAdd);
-                    }
-                    else if(ports.containsKey(toExecute.getParam1())) {
-                        ports.put(toExecute.getParam1(),intNumberToAdd);
-                    }
-                } catch (Exception e) {
-                    System.out.println("Błąd w mov");
-                    throw new InstructionException();
-                }
-            }
-            else {
-                System.out.println("Błąd w mov");
-                throw new InstructionException();
-            }
-            int mnemonicSize = Main.cpu.getMnemonicSize("MOV");
-            linePointer = linePointer + mnemonicSize;
-        }
-        else if(toExecute.getMnemonic().equals("DJNZ")) {
-            try {
-                int wartosc = registers.get(toExecute.getParam1());
-                wartosc--;
-                if(wartosc==-1)
-                    wartosc=255;
-
-                registers.put(toExecute.getParam1(),wartosc);
-
-                if(wartosc!=0) {
-                    linePointer = codeMemory.getLineFromLabel(toExecute.getParam2());
-                }
-                if(wartosc==0) {
-                    int mnemonicSize = Main.cpu.getMnemonicSize("DJNZ");
-                    linePointer += mnemonicSize;
-                }
-            }
-            catch (Exception e) {
-                throw new InstructionException();
+            if(wartosc==0) {
+                linePointer+=3;
             }
         }
+        else if(toExecute.equals("01110100")) { // MOV A,#01h
+            machineCycle();
+            registers.put("A",Integer.parseInt(codeMemory.getFromAddress(linePointer+1),2));
+            linePointer+=2;
+        }
+        else if(toExecute.substring(0,5).equals("11101")) { //MOV A,Rx
+            machineCycle();
+            int rejestr = Integer.parseInt(toExecute.substring(5,7),2);
+            String rejestrString = "R" + rejestr;
+            int wartosc = registers.get(rejestrString);
+            registers.put("A",wartosc);
+            linePointer+=1;
+        }
+        else if(toExecute.equals("11100101")) { //MOV A,Px
+            machineCycle();
+
+            if(codeMemory.getFromAddress(linePointer+1).equals("10000000")) {
+                registers.put("A",ports.get("P0"));
+            }
+            if(codeMemory.getFromAddress(linePointer+1).equals("10010000")) {
+                registers.put("A",ports.get("P1"));
+            }
+            if(codeMemory.getFromAddress(linePointer+1).equals("10100000")) {
+                registers.put("A",ports.get("P2"));
+            }
+            if(codeMemory.getFromAddress(linePointer+1).equals("10110000")) {
+                registers.put("A",ports.get("P3"));
+            }
+            linePointer+=2;
+        }
+        else if(toExecute.substring(0,5).equals("01111")) {
+            machineCycle();
+            int rejestr = Integer.parseInt(toExecute.substring(5,7),2);
+            String rejestrString = "R" + rejestr;
+            int wartosc = Integer.parseInt(codeMemory.getFromAddress(linePointer+1),2);
+            registers.put(rejestrString,wartosc);
+            linePointer+=2;
+        }
+        else if(toExecute.substring(0,5).equals("11111")) {
+            machineCycle();
+            int rejestr = Integer.parseInt(toExecute.substring(5,7),2);
+            String rejestrString = "R" + rejestr;
+            registers.put(rejestrString,registers.get("A"));
+            linePointer+=1;
+        }
+        else if(toExecute.equals("11110101")) {
+            machineCycle();
+            if(codeMemory.getFromAddress(linePointer+1).equals("10000000")) {
+                ports.put("P0",registers.get("A"));
+            }
+            if(codeMemory.getFromAddress(linePointer+1).equals("10010000")) {
+                ports.put("P1",registers.get("A"));
+            }
+            if(codeMemory.getFromAddress(linePointer+1).equals("10100000")) {
+                ports.put("P2",registers.get("A"));
+            }
+            if(codeMemory.getFromAddress(linePointer+1).equals("10110000")) {
+                ports.put("P3",registers.get("A"));
+            }
+            linePointer+=2;
+        }
+        /*
         else if(toExecute.getMnemonic().equals("RL")) {
             if(toExecute.getParam1().equals("A")) {
                 int wartosc = registers.get(toExecute.getParam1());
@@ -276,58 +240,152 @@ public class Cpu {
                 throw new InstructionException();
             }
         }
+        else if(toExecute.getMnemonic().equals("ADD")) {
+            if(toExecute.getParam1().equals("A")) {
+                if(toExecute.getParam2().charAt(0) == '#') {
+                    String numberToAdd = toExecute.getParam2();
+                    int numberToAddLength = numberToAdd.length();
+                    int intNumberToAdd =-1;
+                    if(numberToAdd.charAt(numberToAddLength-1)=='D') {
+                        intNumberToAdd = Integer.parseInt(numberToAdd.substring(1,numberToAddLength-1));
+                    }
+                    else if(numberToAdd.charAt(numberToAddLength-1)=='H') {
+                        intNumberToAdd = Integer.parseInt(numberToAdd.substring(1,numberToAddLength-1),16);
+                    }
+                    else if(numberToAdd.charAt(numberToAddLength-1)=='B') {
+                        intNumberToAdd = Integer.parseInt(numberToAdd.substring(1,numberToAddLength-1),2);
+                    }
+                    else {
+                        System.out.println("Niepomyślne analizowanie liczby");
+                        throw new InstructionException();
+                    }
+                    if(intNumberToAdd!=-1) {
+                        int akumulator = registers.get("A");
+                        int wynik = akumulator + intNumberToAdd;
+                        if(wynik>255) {
+                            wynik-=255;
+                            psw.put("C",true);
+                        }
+                        else {
+                            psw.put("C",false);
+                        }
+                        registers.put("A",wynik);
+
+                    }
+                    else {
+                        throw new InstructionException();
+                    }
+                }
+                else {
+                    int toAdd = -1;
+                    if(registers.containsKey(toExecute.getParam2())) {
+                        toAdd = registers.get(toExecute.getParam2());
+                    }
+                    else if(ports.containsKey(toExecute.getParam2())) {
+                        toAdd = ports.get(toExecute.getParam2());
+                    }
+                    else {
+                        throw new InstructionException();
+                    }
+                    int accInt = registers.get("A");
+                    int wynik = accInt+toAdd;
+
+                    if(wynik>255) {
+                        wynik-=255;
+                        psw.put("C",true);
+                    }
+                    else {
+                        psw.put("C",false);
+                    }
+                    registers.put("A",wynik);
+
+                }
+            } else {
+                throw new InstructionException();
+            }
+
+            int mnemonicSize = Main.cpu.getMnemonicSize("ADD");
+            linePointer += mnemonicSize;
+
+        } else if(toExecute.getMnemonic().equals("ADDC")) {
+            int bitC = psw.get("C") ? 1 : 0;
+            if(toExecute.getParam1().equals("A")) {
+                if(toExecute.getParam2().charAt(0) == '#') {
+                    String numberToAdd = toExecute.getParam2();
+                    int numberToAddLength = numberToAdd.length();
+                    int intNumberToAdd =-1;
+                    if(numberToAdd.charAt(numberToAddLength-1)=='D') {
+                        intNumberToAdd = Integer.parseInt(numberToAdd.substring(1,numberToAddLength-1));
+                    }
+                    else if(numberToAdd.charAt(numberToAddLength-1)=='H') {
+                        intNumberToAdd = Integer.parseInt(numberToAdd.substring(1,numberToAddLength-1),16);
+                    }
+                    else if(numberToAdd.charAt(numberToAddLength-1)=='B') {
+                        intNumberToAdd = Integer.parseInt(numberToAdd.substring(1,numberToAddLength-1),2);
+                    }
+                    else {
+                        System.out.println("Niepomyślne analizowanie liczby");
+                        throw new InstructionException();
+                    }
+                    if(intNumberToAdd!=-1) {
+                        int akumulator = registers.get("A");
+                        int wynik = akumulator + intNumberToAdd + bitC;
+                        if(wynik>255) {
+                            wynik-=255;
+                            psw.put("C",true);
+                        }
+                        else {
+                            psw.put("C",false);
+                        }
+                        registers.put("A",wynik);
+
+                    }
+                    else {
+                        throw new InstructionException();
+                    }
+                }
+                else {
+                    int toAdd = -1;
+                    if(registers.containsKey(toExecute.getParam2())) {
+                        toAdd = registers.get(toExecute.getParam2());
+                    }
+                    else if(ports.containsKey(toExecute.getParam2())) {
+                        toAdd = ports.get(toExecute.getParam2());
+                    }
+                    else {
+                        throw new InstructionException();
+                    }
+                    int accInt = registers.get("A");
+                    int wynik = accInt+toAdd + bitC;
+
+                    if(wynik>255) {
+                        wynik-=255;
+                        psw.put("C",true);
+                    }
+                    else {
+                        psw.put("C",false);
+                    }
+                    registers.put("A",wynik);
+
+                }
+            } else {
+                throw new InstructionException();
+            }
+
+            int mnemonicSize = Main.cpu.getMnemonicSize("ADD");
+            linePointer += mnemonicSize;
+
+        }
 
         int machineCyclesNumber = Main.cpu.getMnemonicTime(toExecute.getMnemonic().toUpperCase());
         for (;machineCyclesNumber>0;machineCyclesNumber--) {
             machineCycle();
         }
-
+*/
         refreshGui();
     }
 
-    public int isMnemonic(String name) {
-        for(Mnemonic mnemonic : knownMnemonics) {
-            if(mnemonic.getName().toUpperCase().equals(name))
-                return (int) mnemonic.getParamsNumber();
-        }
-        return -1;
-    }
-    public int getMnemonicTime(String name) {
-        for(Mnemonic mnemonic : knownMnemonics) {
-            if(mnemonic.getName().toUpperCase().equals(name))
-                return (int) mnemonic.getTime();
-        }
-        return 0;
-    }
-    public int getMnemonicSize(String name) {
-        for(Mnemonic mnemonic : knownMnemonics) {
-            if(mnemonic.getName().toUpperCase().equals(name))
-                return (int) mnemonic.getSize();
-        }
-        return 0;
-    }
-    private int linePointer;
-
-
-    private Map<String,Integer> registers = new HashMap<>();
-
-    private Map<String,Boolean> psw = new HashMap<>();
-
-    public CodeMemory codeMemory = new CodeMemory();
-
-    public int getLinePointer() {
-        return linePointer;
-    }
-
-    public void setLinePointer(int linePointer) {
-        this.linePointer = linePointer;
-    }
-
-    private Map<String,Integer> ports = new HashMap<>();
-
-    private ArrayList<Mnemonic> knownMnemonics = new ArrayList<>();
-
-    public void refreshStatusRegister(){
+    private void refreshStatusRegister(){
         String acc = Integer.toBinaryString(registers.get("A"));
         if(acc.length()>4)
             psw.put("AC",true);
@@ -440,4 +498,10 @@ public class Cpu {
         }
         return number;
     }
+
+    private int linePointer;
+    private Map<String,Integer> registers = new HashMap<>();
+    private Map<String,Boolean> psw = new HashMap<>();
+    public CodeMemory codeMemory = new CodeMemory();
+    private Map<String,Integer> ports = new HashMap<>();
 }
